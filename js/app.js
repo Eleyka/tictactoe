f86bd7ee26a87c95517c9e60360ec6eeea847bc7
Eleyka <eleynekrd@gmail.com>
2017-11-09 08:05:20 -0500

    subiendo archivos

---
+++ js/app.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    var board = document.querySelector('.board-js');
+  
+    board.addEventListener('click', addX);
+  };
+  
+  var centinel = true;
+  
+  function addX(event) {
+    if (centinel) {
+    event.target.textContent = 'X';
+    centinel = false;
+    } else {
+    event.target.textContent = 'O';
+    centinel = true;
+    }
+  }
+  
+  function resed(){
+ 
+    board.addEventListener('click', removeEventListener);
+  }
+  
+  
+  
+  //   function(event) {
+  //     // event.target.style.backgroundColor='yellow';
+  //     // event.target.style.border = '1px';
+  //     event.target.innerHTML = '<h1>X</h1>';
+  //   });
+  // };
